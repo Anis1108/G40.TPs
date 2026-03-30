9e158d23586695e1ee39ddf1676cce6b0933955d
Anis1108 <ton_email_github@example.com>
2026-03-30 11:30:29 +0200

    TP4 bonus nombres romains

--- TP4/roman_to_int.py
+++ TP4/roman_to_int.py
@@ -25,5 +25,45 @@ def roman_to_int(s):
     return total
 
 
-roman = input("Entrer un nombre romain : ")
-print("Valeur entière :", roman_to_int(roman))
+def int_to_roman(num):
+    valeurs = [
+        (1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I")
+    ]
+
+    resultat = ""
+
+    for val, sym in valeurs:
+        while num >= val:
+            resultat += sym
+            num -= val
+
+    return resultat
+
+
+print("1 - Romain vers entier")
+print("2 - Entier vers romain")
+
+choix = input("Choix : ")
+
+if choix == "1":
+    roman = input("Entrer un nombre romain : ")
+    print("Valeur entière :", roman_to_int(roman))
+
+elif choix == "2":
+    entier = int(input("Entrer un entier : "))
+    print("Nombre romain :", int_to_roman(entier))
+
+else:
+    print("Choix invalide")
